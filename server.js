816447beaa43abb81cef975ea02f993064ef2b18
Genevieve Ferrari <genferrari@gmail.com>
2022-12-11 22:45:58 -0800

    organized folders

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const fs = require("fs");
+
+const PORT = 3001;
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
