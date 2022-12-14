b457daa6768bf78836fe6422be34a925ec939330
Genevieve Ferrari <genferrari@gmail.com>
2022-12-13 23:23:28 -0800

    added additional basic code

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
 const PORT = 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//creates route for public folder
+app.use(express.static("public"));
+
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
+
+//Need more time on homework
