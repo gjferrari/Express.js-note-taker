3848f70cef93e1d2f29d565f2523aec7a2cee857
Genevieve Ferrari <genferrari@gmail.com>
2023-03-05 00:28:09 -0800

    added basic apis - untested

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -2,12 +2,27 @@ const path = require("path");
 const router = require("express").Router();
 const fs = require("fs");
 
-const createNewNote = (body, notesArray) => {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notesArray })
-  );
-  return note;
-};
+// const createNewNote = (body, notesArray) => {
+//   const note = body;
+//   notesArray.push(note);
+//   fs.writeFileSync(
+//     path.join(__dirname, "../db/db.json"),
+//     JSON.stringify({ notesArray })
+//   );
+//   return note;
+// }
+
+router.get("api/notes/:id", (req, res) => {
+  res.json(notes[req.params.id]);
+});
+
+router.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+router.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8");
+});
