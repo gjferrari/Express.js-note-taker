6bd3f2c118daba0f9e778c017cafa071d91734dc
Genevieve Ferrari <genferrari@gmail.com>
2023-03-05 15:12:36 -0800

    consolidating code into server.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 
 //code borrowed from Project 2
-const apiRoutes = require("./api");
-const htmlRoutes = require("./api");
+const apiRoutes = require("./api/apiRoutes");
+const htmlRoutes = require("./api/htmlRoutes");
 
 router.use("/api", apiRoutes);
 router.use("/htmlRoutes", htmlRoutes);
+
+router.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+router.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "public/index.html"));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
+const router = require("express").Router();
 const fs = require("fs");
 const path = require("path");
-const apiRoutes = require("./controllers/api");
-const htmlRoutes = require("./controllers/api");
+// const apiRoutes = require("./controllers/api");
+// const htmlRoutes = require("./controllers/api");
 
-const app = express();
+// const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
 //creates route for public folder
-app.use(express.static("public"));
+router.use(express.static("public"));
 
-//use api routes
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// //use api routes
+// router.use("/api", apiRoutes);
+// router.use("/", htmlRoutes);
 
-app.listen(PORT, () =>
+router.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
