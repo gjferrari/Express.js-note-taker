8d659883c1f719007bc5ea3c383b89516328ac1a
Genevieve Ferrari <genferrari@gmail.com>
2023-03-15 22:54:24 -0700

    correct code by adding const dbPath and debugged get route

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -3,39 +3,63 @@ const router = require("express").Router();
 const fs = require("fs");
 const { resolve } = require("path");
 
+const dbPath = "db/db.json";
+
 const notes = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(jsonPath, "utf8", (err, data) => {
+    fs.readFile(dbPath, "utf8", (err, data) => {
       if (err) {
-        reject(jsonData);
+        reject(dbPath);
       } else {
         resolve(JSON.parse(data));
       }
     });
   });
 };
-const createNewNote = (body, notesArray) => {
-  const note = body;
-  notesArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"),
-    JSON.stringify({ notesArray })
-  );
-  return note;
+const newNote = (data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(dbPath, JSON.stringify(data), "utf-8", (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(true);
+      }
+    });
+  });
 };
 
-router.get("api/notes/:id", (req, res) => {
-  res.json(notes[req.params.id]);
+router.get("/notes", (req, res) => {
+  notes()
+    .then((notes) => res.json(notes))
+    .catch((err) =>
+      res.status(500).json({
+        error: err,
+      })
+    );
 });
 
-router.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    var notes = JSON.parse(data);
-    res.json(notes);
-  });
+router.post("/notes", (req, res) => {
+  newNote()
+    .then((newNote) => res.json(newNote))
+    .catch((err) =>
+      res.status(500).json({
+        error: err,
+      })
+    );
 });
-router.post("/api/notes", (req, res) => {
+
+// router.get("api/notes/:id", (req, res) => {
+//   res.json(notes[req.params.id]);
+// });
+
+// router.get("/notes", (req, res) => {
+//   fs.readFile("./db/db.json", "utf8", (err, data) => {
+//     if (err) throw err;
+//     var notes = JSON.parse(data);
+//     res.json(notes);
+//   });
+// });
+router.post("/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf-8");
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -5,19 +5,41 @@ const path = require("path");
 const app = express();
 
 const apiRoutes = require("./controllers/api/apiRoutes");
-const htmlRoutes = require("./controllers/api/htmlRoutes");
+// const htmlRoutes = require("./controllers/api/htmlRoutes");
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
-// Route for HTML calls
-app.use("/", htmlRoutes);
+// // Route for HTML calls
+// app.use("/", htmlRoutes);
 
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
+});
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8");
+});
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 // verifies the server connected to the port properly
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
