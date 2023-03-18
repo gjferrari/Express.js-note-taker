3ee2c3147da7467ae5884227a5aacf3396aea60f
Genevieve Ferrari <genferrari@gmail.com>
2023-03-17 17:55:55 -0700

    deleted old code

--- server.js
+++ server.js
@@ -23,11 +23,6 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-// app.post("/api/notes", (req, res) => {
-//   const notes = db;
-//   console.log(req.body);
-// });
-
 app.post("/api/notes", (req, res) => {
   console.log("post note");
   console.log(req.body);
@@ -60,62 +55,6 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public/index.html"));
-// });
-// verifies the server connected to the port properly
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
-// const express = require("express");
-// const router = require("express").Router();
-// const fs = require("fs");
-// const path = require("path");
-// // const apiRoutes = require("./controllers/api");
-// // const htmlRoutes = require("./controllers/api");
-
-// // const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Middleware for parsing JSON and urlencoded form data
-// router.use(express.json());
-// router.use(express.urlencoded({ extended: true }));
-
-// //creates route for public folder
-// router.use(express.static("public"));
-
-// // //use api routes
-// // router.use("/api", apiRoutes);
-// // router.use("/", htmlRoutes);
-
-// router.get("api/notes/:id", (req, res) => {
-//   res.json(notes[req.params.id]);
-// });
-
-// router.get("/api/notes", (req, res) => {
-//   fs.readFile("./db/db.json", "utf8", (err, data) => {
-//     if (err) throw err;
-//     var notes = JSON.parse(data);
-//     res.json(notes);
-//   });
-// });
-// router.post("/api/notes", (req, res) => {
-//   fs.readFile("./db/db.json", "utf-8");
-// });
-
-// router.get("/", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/public/index.html"))
-// );
-
-// router.get("/notes", (req, res) =>
-//   res.sendFile(path.join(__dirname, "/public/notes.html"))
-// );
-
-// router.get("*", (req, res) => {
-//   res.sendFile(path.join(_dirname, "public/index.html"));
-// });
-
-// router.listen(PORT, () =>
-//   console.log(`Example app listening at http://localhost:${PORT}`)
-// );
